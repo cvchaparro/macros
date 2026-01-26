55f49d415372a31e22483586d575d16d94ac274b
Cameron Chaparro <cameron@cameronchaparro.com>
2026-01-25 18:52:47 -0600

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil
+  . ((projectile-create-missing-test-files . t)))
+ (clojure-mode
+  . ((cider-clojure-cli-aliases . "dev:cider")
+     (cider-inject-dependencies-at-jack-in . nil)
+     (cider-jack-in-cmd
+      . "/usr/bin/clojure -M:lib/sayid:dev/reloaded:dev:cider"))))
