1e0d97bb7e0cbf61495700ed704752337d05a1d5
Cameron Chaparro <cameron@cameronchaparro.com>
2026-02-03 13:51:47 -0600

    Organize imports

--- src/io/cvcf/ui/cli/entrypoint.clj
+++ src/io/cvcf/ui/cli/entrypoint.clj
@@ -1,11 +1,11 @@
 (ns io.cvcf.ui.cli.entrypoint
   (:require
    [babashka.cli :as cli]
-   [io.cvcf.ui.cli.core :refer [with-log]]
-   [io.cvcf.ui.cli.cmds.import :refer [commands] :rename {commands import-commands}]
    [io.cvcf.ui.cli.cmds.export :refer [commands] :rename {commands export-commands}]
+   [io.cvcf.ui.cli.cmds.import :refer [commands] :rename {commands import-commands}]
+   [io.cvcf.ui.cli.cmds.log :refer [commands] :rename {commands log-commands}]
    [io.cvcf.ui.cli.cmds.new :refer [commands] :rename {commands new-commands}]
-   [io.cvcf.ui.cli.cmds.log :refer [commands] :rename {commands log-commands}]))
+   [io.cvcf.ui.cli.core :refer [with-log]]))
 
 (def commands
   (concat
